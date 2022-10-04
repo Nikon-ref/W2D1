c4bd0cad88381d81eae9e48c25be6c4d28203810
Nikon-ref <jetnikon@gmail.com>
2022-10-04 15:58:54 +0100

    Extra Homework try

---
+++ extraindex.js
@@ -0,0 +1,27 @@
+console.log("------Spaghetti alla Carbonara Recipe------");
+
+let spaghetti = 400;
+let guanciale = 250;
+let veryFreshEggYolks = 6;
+let PecorinoCheese = 50;
+let rawBlackPepper = 4;
+
+console.log(
+  "To make classic carbonara, first cut the guanciale into 1cm layers, then into long strips: ",
+  guanciale
+);
+let eggWithPecorinoCheese = veryFreshEggYolks + PecorinoCheese;
+console.log(
+  "Combine the egg yolks with the finely grated Pecorino Romano: ",
+  eggWithPecorinoCheese
+);
+
+let BlackPepperRoast = rawBlackPepper / 4 + eggWithPecorinoCheese;
+console.log(
+  "Roast the black pepper on a pan until it smokes and sizzles, and combine a small amount of it to the yolks and chees mixture, set aside the rest: ",
+  BlackPepperRoast
+);
+
+console.log(
+  "Brown the strips of guanciale for 3 minutes at medium heat, then 1 minune on high heat til crisp, then turn off the heat, take out the guanciale, leaving only the grease on the pan cool down."
+);
