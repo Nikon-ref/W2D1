1218bea90b997fcfa39a6ba94358777a54930c92
Nikon-ref <jetnikon@gmail.com>
2022-10-04 15:33:26 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+let firstNameVariable = "Farben"; //fixed bug with vertical commas or can't add due to values not being numbers
+let secondNameVariable = "Nikon";
+let surnameVariable = "Ahamed";
+let space = " ";
+let fullName = firstNameVariable + space + secondNameVariable;
+let fullNameWithSurname = fullName + space + surnameVariable;
+//assigned value only
+//empty run
+console.log("Hello, my name is: ", fullNameWithSurname);
+
+let myAge = 23;
+let currentYear = 2022;
+let birthYear = currentYear - myAge;
+
+console.log("My year of birth is: ", birthYear);
